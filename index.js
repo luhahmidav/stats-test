ffa0ba7d92377af4251ed64efdeaeb1df6bf4daf
luhahmidav <starassov@leongaming.com>
2018-01-18 23:53:32 +0200

    mail

--- index.js
+++ index.js
@@ -2,22 +2,35 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const sendmail = require('sendmail')();
 
 var fs = require('fs');
 var util = require('util');
-var d = new Date();
-var suffix = [d.getFullYear(), d.getMonth()+1, d.getDate()].join('-');
-var logPath = path.join(__dirname, 'public') + '/q1-' + suffix + '.log';
-var writeStream = fs.createWriteStream(logPath, {flags : 'w'});
+// var d = new Date();
+// var suffix = [d.getFullYear(), d.getMonth()+1, d.getDate()].join('-');
+// var logPath = path.join(__dirname, 'public') + '/q1-' + suffix + '.log';
+// var writeStream = fs.createWriteStream(logPath, {flags : 'w'});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(function(req, res, next) {
-    if (req.query.q1log) {
-      res.end(fs.readFileSync(logPath, {encoding : 'utf8'}));
-    }
-    else if (req.query.q1) {
-      writeStream.write(req.query.q1 + '\n');
+    // if (req.query.q1log) {
+    //   res.end(fs.readFileSync(logPath, {encoding : 'utf8'}));
+    // }
+    // else
+    if (req.query.q1) {
+ 
+      sendmail({
+          from: 'no-reply@stats-universe.herokuapp.com',
+          to: 'vadimhahhuli@gmail.com',
+          subject: 'q1',
+          html: req.query.q1,
+        }, function(err, reply) {
+          console.log(err && err.stack);
+          console.dir(reply);
+      });
+
+      // writeStream.write(req.query.q1 + '\n');
       res.end('');
     }
     else {
