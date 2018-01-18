9bd9d0726dba2206786eb982cfe39adf8f0c8ca5
luhahmidav <starassov@leongaming.com>
2018-01-18 22:40:22 +0200

    fix

--- index.js
+++ index.js
@@ -7,19 +7,23 @@ var fs = require('fs');
 var util = require('util');
 var d = new Date();
 var suffix = [d.getFullYear(), d.getMonth()+1, d.getDate()].join('-');
-
-var log_file = fs.createWriteStream((path.join(__dirname, 'public') + '/q1-' + suffix + '.log', {flags : 'w'}));
-// var log_stdout = process.stdout;
-
+var logPath = path.join(__dirname, 'public') + '/q1-' + suffix + '.log';
+var writeStream = fs.createWriteStream(logPath, {flags : 'w'});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(function(req, res, next) {
-    if (req.query.q1) log_file.write(req.query.q1 + '\n');
-
+    if (req.query.q1log) {
+      res.end(fs.readFileSync(logPath, {encoding : 'utf8'}));
+    }
+    else if (req.query.q1) {
+      writeStream.write(req.query.q1 + '\n');
+      res.end('');
+    }
+    else {
+      res.end('');
+    }
     next();
   })
-//  .set('views', path.join(__dirname, 'views'))
-//  .set('view engine', 'ejs')
-//  .get('/', (req, res) => res.render('pages/index'))
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
